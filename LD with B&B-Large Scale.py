0f7203444d8b7e7c6d18dc5d2cef554df692f175
Farzane <ezzati.f.111@gmail.com>
2023-11-24 12:14:06 -0600

    Updates

--- LD with B&B-Large Scale.py
+++ LD with B&B-Large Scale.py
@@ -401,7 +401,7 @@ class Model:
         with open('ToySolution.pkl', 'wb') as handle:
             pickle.dump([x_opt, y_pves, y_dges, y_grides, y_pvl, y_dgl, y_esl, y_gridl, y_l, y_lh, y_ll, y_pvcur,
                          y_dgcur, y_pvgrid, y_dggrid, y_esgrid, y_gridplus, y_gridminus, e, pv, self.Ytg_indices,
-                         self.Ytg_indices, self.Yh_indices, zstar], handle)
+                         self.Ytg_indices, self.Yh_indices, self.RNGTime, zstar], handle)
         handle.close()
 
 if __name__ == '__main__':
